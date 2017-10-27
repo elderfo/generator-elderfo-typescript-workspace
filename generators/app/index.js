8a410f271e58e7c7e3218ba211b98f6324a30d61
Chris Getsfred <cgetsfred@gmail.com>
2017-10-27 09:58:52 -0500

    feat: initial development

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,16 +6,26 @@ const yosay = require('yosay');
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the transcendent ' + chalk.red('generator-elderfo-typescript-workspace') + ' generator!'
-    ));
+    this.log(
+      yosay(
+        'Welcome to the transcendent ' +
+          chalk.red('generator-elderfo-typescript-workspace') +
+          ' generator!'
+      )
+    );
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    const prompts = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Application name',
+      },
+      {
+        type: 'input',
+        name: 'version',
+        message: 'Application version',
+      },
+    ];
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
@@ -25,12 +35,36 @@ module.exports = class extends Generator {
 
   writing() {
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath('jest.json'),
+      this.destinationPath(`${this.props.name}/jest.json`)
+    );
+
+    this.fs.copy(
+      this.templatePath('tsconfig.json'),
+      this.destinationPath(`${this.props.name}/tsconfig.json`)
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('_lerna.json'),
+      this.destinationPath(`${this.props.name}/lerna.json`),
+      { version: this.props.version }
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('_package.json'),
+      this.destinationPath(`${this.props.name}/package.json`),
+      {
+        version: this.props.version,
+        name: this.props.name,
+      }
     );
   }
 
   install() {
-    this.installDependencies();
+    this.installDependencies({
+      bower: false,
+      npm: false,
+      yarn: true,
+    });
   }
 };
