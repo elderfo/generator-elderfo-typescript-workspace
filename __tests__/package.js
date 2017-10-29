a31aaded9846144ac3cf1aa1ca497cc0561fd2c1
Chris Getsfred <cgetsfred@gmail.com>
2017-10-29 09:34:09 -0500

    test: switch to promises

--- __tests__/package.js
+++ __tests__/package.js
@@ -8,51 +8,47 @@ describe('generator-elderfo-typescript-workspace:package', () => {
   const appName = faker.random.word();
   const appVersion = faker.random.number();
 
-  beforeAll(() => {
+  it('should assign name and version to package.json', () => {
     return helpers
       .run(path.join(__dirname, '../generators/package'))
       .withOptions({ 'skip-bootstrap': true })
-      .withPrompts({ name: appName, version: appVersion });
+      .withPrompts({ name: appName, version: appVersion })
+      .then(() => {
+        assert.jsonFileContent(`packages/${appName}/package.json`, {
+          name: appName,
+          version: appVersion,
+        });
+      });
   });
 
-  it('should assign name and version to package.json', async () => {
-    await helpers
-      .run(path.join(__dirname, '../generators/package'))
-      .withOptions({ 'skip-bootstrap': true })
-      .withPrompts({ name: appName, version: appVersion });
-
-    assert.jsonFileContent(`packages/${appName}/package.json`, {
-      name: appName,
-      version: appVersion,
-    });
-  });
-
-  it('should assign scope to package.json', async () => {
-    await helpers
+  it('should assign scope to package.json', () => {
+    return helpers
       .run(path.join(__dirname, '../generators/package'))
       .withOptions({ 'skip-bootstrap': true })
       .withPrompts({
         name: appName,
         version: appVersion,
         packageScope: 'abc123',
+      })
+      .then(() => {
+        assert.jsonFileContent(`packages/${appName}/package.json`, {
+          name: `abc123/${appName}`,
+          version: appVersion,
+        });
       });
-
-    assert.jsonFileContent(`packages/${appName}/package.json`, {
-      name: `abc123/${appName}`,
-      version: appVersion,
-    });
   });
 
-  it('should copy non-template files', async () => {
-    await helpers
+  it('should copy non-template files', () => {
+    return helpers
       .run(path.join(__dirname, '../generators/package'))
       .withOptions({ 'skip-bootstrap': true })
-      .withPrompts({ name: appName, version: appVersion });
-
-    assert.file([
-      `packages/${appName}/tsconfig.json`,
-      `packages/${appName}/__tests__/index.test.ts`,
-      `packages/${appName}/src/index.ts`,
-    ]);
+      .withPrompts({ name: appName, version: appVersion })
+      .then(() => {
+        assert.file([
+          `packages/${appName}/tsconfig.json`,
+          `packages/${appName}/__tests__/index.test.ts`,
+          `packages/${appName}/src/index.ts`,
+        ]);
+      });
   });
 });
